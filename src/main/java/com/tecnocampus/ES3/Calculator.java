39d697811faa5e266bdcc71e8ed92eaf8ddd39e7
polrios <133013642+polrios@users.noreply.github.com>
2023-05-09 14:35:36 +0200

    TEST 0

---
+++ src/main/java/com/tecnocampus/ES3/Calculator.java
@@ -0,0 +1,16 @@
+package com.tecnocampus.ES3;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    public void processToken(char token) {
+
+    }
+
+    public String getDisplay() {
+        return "";
+    }
+}
